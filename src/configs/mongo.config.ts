9af6da78d2888b81db5bc9de71fa78664602d59c
dmitriy <d.12.09.2002.d@gmail.com>
2022-07-05 17:41:42 +0300

    ошибка вывода

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoConfig = async (ConfigService: ConfigService): Promise<TypegooseModuleOptions> => {
+	return {
+		uri: getMongoString(ConfigService),
+		...getMongoOptions()
+	}
+}
+const getMongoString = (ConfigService: ConfigService) =>
+	'mongodb://' +
+	ConfigService.get('MONGO_LOGIN') +
+	':' +
+	ConfigService.get('MONGO_PASSWORD') +
+	'@' +
+	ConfigService.get('MONGO_HOST') +
+	':' +
+	ConfigService.get('MONGO_PORT') +
+	'/' +
+	ConfigService.get('MONGO_AUTHDATABASE')
+
+const getMongoOptions = () => ({
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+})
