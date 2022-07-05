84fde0e8c95422bfbbb098a67dc8c3d3a304da5d
dmitriy <d.12.09.2002.d@gmail.com>
2022-07-05 17:35:51 +0300

    ошибка вывода

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,21 @@
-export class ReviewModel {
-  _id: string;
-  name: string;
-  title: string;
-  description: string;
-  rating: number;
-  createdAt: Date;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { Types } from 'mongoose';
+
+export interface ReviewModel extends Base { }
+export class ReviewModel extends TimeStamps {
+	@prop()
+	name: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	description: string;
+
+	@prop()
+	rating: number;
+
+	@prop()
+	productId: Types.ObjectId;
 }
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,25 +1,64 @@
+import { prop, index } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
 export enum TopLevelCategory {
-  Courses,
-  Services,
-  Books,
-  Products
+	Courses,
+	Services,
+	Books,
+	Products
 }
-export class TopPageModel {
-  _id: string;
-  firstLevelCategory: TopLevelCategory;
-  secondCategory: string;
-  title: string;
-  category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
-  seoText: string;
-  tags: string[];
+
+export class HhData {
+	@prop()
+	count: number;
+
+	@prop()
+	juniorSalary: number;
+
+	@prop()
+	middleSalary: number;
+
+	@prop()
+	seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+	@prop()
+	title: string;
+
+	@prop()
+	description: string;
+}
+
+export interface TopPageModel extends Base { }
+export class TopPageModel extends TimeStamps {
+	@prop({ enum: TopLevelCategory })
+	firstCategory: TopLevelCategory;
+
+	@prop()
+	secondCategory: string;
+
+	@prop({ unique: true })
+	alias: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	category: string;
+
+	@prop({ type: () => HhData })
+	hh?: HhData;
+
+	@prop({ type: () => [TopPageAdvantage] })
+	advantages?: TopPageAdvantage[];
+
+	@prop()
+	seoText?: string;
+
+	@prop()
+	tagsTitle: string;
+
+	@prop({ type: () => [String] })
+	tags: string[];
 }
